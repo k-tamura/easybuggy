7cb72eb34b82c62c679970a9fb60f739f2f4a5ef
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-01-05 13:09:15 +0900

    Add a servlet as template.

---
+++ src/main/java/org/t246osslab/easybuggy/TestServlet.java
@@ -0,0 +1,21 @@
+package org.t246osslab.easybuggy;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.t246osslab.easybuggy.utils.HTTPResponseCreator;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = { "/test" })
+public class TestServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+        HTTPResponseCreator.createSimpleResponse(res, "Test", "Test!!");
+    }
+}
