a2cc9fdc115c5414a1b7489b8f9c5d806926f759
k-tamura <ktamura.biz.80@gmail.com>
2017-02-13 22:40:56 +0900

    Improve JVMCrashByEAVServlet

--- src/main/java/org/t246osslab/easybuggy/troubles/JVMCrashByEAVServlet.java
+++ src/main/java/org/t246osslab/easybuggy/troubles/JVMCrashByEAVServlet.java
@@ -1,8 +1,7 @@
 package org.t246osslab.easybuggy.troubles;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,31 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.pmw.tinylog.Logger;
-import org.t246osslab.easybuggy.utils.Closer;
-import org.t246osslab.easybuggy.utils.HTTPResponseCreator;
-import org.t246osslab.easybuggy.utils.MessageUtils;
 
-//import sun.dc.path.PathConsumer;
-//import sun.dc.pr.PathDasher;
+import sun.misc.Unsafe;
 
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = { "/jvmcrasheav" })
 public class JVMCrashByEAVServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        // PathDasher dasher = new PathDasher(null);
-        PrintWriter writer = null;
+
         try {
-            Class<?> cl = Class.forName("sun.dc.pr.PathDasher");
-            Class<?> cl2 = Class.forName("sun.dc.path.PathConsumer");
-            Constructor<?> cunstructor = cl.getConstructor(new Class[] { cl2 });
-            cunstructor.newInstance(new Object[] { null });
+            getUnsafe().getByte(0);
         } catch (Exception e) {
             Logger.error(e);
-            HTTPResponseCreator.createSimpleResponse(res, null,
-                    MessageUtils.getMsg("msg.info.jvm.not.crash", req.getLocale()));
-        } finally {
-            Closer.close(writer);
         }
+    }
+
+    private static Unsafe getUnsafe()
+            throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+        Field singleoneInstanceField = Unsafe.class.getDeclaredField("theUnsafe");
+        singleoneInstanceField.setAccessible(true);
+        return (Unsafe) singleoneInstanceField.get(null);
     }
 }
