d1010b563be36ba0e8989d959aefc6d22e1792d9
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-03-21 14:41:20 +0900

    Change implementation to support for Java 6

--- src/main/java/org/t246osslab/easybuggy/core/utils/Closer.java
+++ src/main/java/org/t246osslab/easybuggy/core/utils/Closer.java
@@ -1,6 +1,11 @@
 package org.t246osslab.easybuggy.core.utils;
 
+import java.io.Closeable;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,23 +23,88 @@ public final class Closer {
     }
 
     /**
+     * Close a Connection object.
+     * 
+     * @param conn Connection object.
+     */
+    public static void close(Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                log.error("IOException occurs: ", e);
+            }
+        }
+    }
+
+    /**
+     * Close a Statement object.
+     * 
+     * @param stmt Statement object.
+     */
+    public static void close(Statement stmt) {
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.error("IOException occurs: ", e);
+            }
+        }
+    }
+
+    /**
+     * Close a ResultSet object.
+     * 
+     * @param rs ResultSet object.
+     */
+    public static void close(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.error("IOException occurs: ", e);
+            }
+        }
+    }
+
+    /**
      * Close all Closeable objects.
-     *
+     * 
      * @param closeables Closeable objects.
      */
-    public static void close(AutoCloseable... closeables) {
+    public static void close(Closeable... closeables) {
         if (closeables != null) {
-            for (AutoCloseable closeable : closeables) {
+            for (Closeable closeable : closeables) {
                 try {
-                    if(closeable != null){
+                    if (closeable != null) {
                         closeable.close();
                     }
                 } catch (IOException e) {
                     log.error("IOException occurs: ", e);
-                } catch (Exception e) {
-                    log.error("Exception occurs: ", e);
                 }
             }
         }
     }
+    
+// for jdk 7 or later
+//    /**
+//     * Close all Closeable objects.
+//     *
+//     * @param closeables Closeable objects.
+//     */
+//    public static void close(AutoCloseable... closeables) {
+//        if (closeables != null) {
+//            for (AutoCloseable closeable : closeables) {
+//                try {
+//                    if(closeable != null){
+//                        closeable.close();
+//                    }
+//                } catch (IOException e) {
+//                    log.error("IOException occurs: ", e);
+//                } catch (Exception e) {
+//                    log.error("Exception occurs: ", e);
+//                }
+//            }
+//        }
+//    }
 }
