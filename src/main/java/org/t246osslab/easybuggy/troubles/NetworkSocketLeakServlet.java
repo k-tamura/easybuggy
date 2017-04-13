02fc617719d02375215465862bf0c63525717fe9
Kohei Tamura <ktamura.biz.80@gmail.com>
2017-04-13 17:40:01 +0900

    Change the fake feature for NetworkSocketLeakServlet

--- src/main/java/org/t246osslab/easybuggy/troubles/NetworkSocketLeakServlet.java
+++ src/main/java/org/t246osslab/easybuggy/troubles/NetworkSocketLeakServlet.java
@@ -1,11 +1,8 @@
 package org.t246osslab.easybuggy.troubles;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.ServletException;
@@ -16,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.t246osslab.easybuggy.core.utils.Closer;
 import org.t246osslab.easybuggy.core.utils.HTTPResponseCreator;
 import org.t246osslab.easybuggy.core.utils.MessageUtils;
 
@@ -29,39 +25,39 @@ public class NetworkSocketLeakServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         HttpURLConnection connection = null;
-        InputStreamReader isr = null;
-        BufferedReader reader = null;
+        URL url = null;
         StringBuilder bodyHtml = new StringBuilder();
         Locale locale = req.getLocale();
-        bodyHtml.append(MessageUtils.getMsg("label.current.time", locale) + ": ");
-        bodyHtml.append(new Date());
-        bodyHtml.append("<br><br>");
         try {
-            URL url = new URL(req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/ping");
+            String pingURL = req.getParameter("pingurl");
+            if (pingURL == null) {
+                pingURL = req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + "/ping";
+            }
+            url = new URL(pingURL);
+
+            long start = System.currentTimeMillis();
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            long end = System.currentTimeMillis();
+            
+            bodyHtml.append("<p>"+MessageUtils.getMsg("description.response.time", req.getLocale())+"</p>");
+            bodyHtml.append("<table class=\"table table-striped table-bordered table-hover\" style=\"font-size:small;\">");
+            bodyHtml.append("<tr><td>" + MessageUtils.getMsg("label.ping.url", locale) + "</td>");
+            bodyHtml.append("<td>" + pingURL + "</td></tr>");
+            bodyHtml.append("<tr><td>" + MessageUtils.getMsg("label.response.code", req.getLocale()) + "</td>");
+            bodyHtml.append("<td>" + responseCode + "</td></tr>");
+            bodyHtml.append("<tr><td>" + MessageUtils.getMsg("label.response.time", locale) + "</td>");
+            bodyHtml.append("<td>" + (end - start) + "</td></tr>");
+            bodyHtml.append("</table>");
 
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                log.error("Unexpected response, HTTP response code: {}", connection.getResponseCode());
-            } else {
-                // isr = new InputStreamReader(connection.getInputStream());
-                // reader = new BufferedReader(isr);
-                // String line = null;
-                // while ((line = reader.readLine()) != null) {
-                // sb.append(line);
-                // }
-            }
             bodyHtml.append(MessageUtils.getInfoMsg("msg.socket.leak.occur", req.getLocale()));
         } catch (Exception e) {
             log.error("Exception occurs: ", e);
             bodyHtml.append(MessageUtils.getErrMsg("msg.unknown.exception.occur", new String[] { e.getMessage() },
                     locale));
         } finally {
-            Closer.close(isr, reader);
-            if (connection != null) {
-                // connection.disconnect();
-            }
-            HTTPResponseCreator.createSimpleResponse(req, res, MessageUtils.getMsg("title.current.time", locale),
+            HTTPResponseCreator.createSimpleResponse(req, res, MessageUtils.getMsg("title.response.time", locale),
                     bodyHtml.toString());
         }
     }
