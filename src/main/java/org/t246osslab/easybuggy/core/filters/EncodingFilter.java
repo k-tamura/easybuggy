0f2ce08ad452bef185a1e49cc48a52653df2f766
Yonatan Geller <yonatan.geller@mend.io>
2026-05-17 16:14:55 +0300

    kjhbluyv

--- src/main/java/org/t246osslab/easybuggy/core/filters/EncodingFilter.java
+++ src/main/java/org/t246osslab/easybuggy/core/filters/EncodingFilter.java
@@ -32,6 +32,7 @@ public class EncodingFilter implements Filter {
             req.setCharacterEncoding("UTF-8");
             res.setContentType("text/html; charset=UTF-8");
         }
+                
         chain.doFilter(req, res);
     }
 
