638a4ea051776d38c84eb284a516957e44900d65
Yonatan Geller <yonatan.geller@mend.io>
2026-05-20 14:11:04 +0300

    ergewe

--- src/main/java/org/t246osslab/easybuggy/core/dao/DBClient.java
+++ src/main/java/org/t246osslab/easybuggy/core/dao/DBClient.java
@@ -12,6 +12,10 @@ import org.slf4j.LoggerFactory;
 import org.t246osslab.easybuggy.core.utils.ApplicationUtils;
 import org.t246osslab.easybuggy.core.utils.Closer;
 
+
+
+
+
 /**
  * Database client to provide database connections.
  */
